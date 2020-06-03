9cfadf88c2a487dfc34437558d6ce70d6d822943
Chris Yoon <chy.yoon@gmail.com>
2020-06-03 15:37:33 -0700

    it will add a select option to the website co-authored-by: Sean Downs <spdowns@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,12 @@ $(document).ready(function() {
     event.preventDefault();
     const nameInput = $("input#name").val();
     const ageInput = $("input#age").val();
+    //console.log(ageInput);
+    const articleInput= $("#article").val(); 
 
     $("#nameSpan").text(nameInput);
     $("#ageSpan").text(ageInput);
+    $("#articleSpan").text(articleInput);
+
   });
 });
